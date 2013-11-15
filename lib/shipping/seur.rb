d35876b302a96cfa00b44a78cef4a72c02fc8d23
AlejandroJL <alejandrojl@me.com>
2013-11-15 14:49:23 +0100

    Fix merge

--- lib/shipping/seur.rb
+++ lib/shipping/seur.rb
@@ -70,10 +70,6 @@ module Shipping
       end
 
       @data = sendXML
-<<<<<<< HEAD
-      @logger.debug("SEUR_DATA:#{@data}")
-=======
->>>>>>> refs/heads/features/seur
       get_seur_response('http://cit.seur.com/CIT-war/services/ImprimirECBWebService?wsdl', 'impresion_integracion_pdf_con_ecbws', 'imp')
     end
 
